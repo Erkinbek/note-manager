a7a6ef2cc282750dd1f7f6c500b3bcc4bc464a0a
Erkin Pardayev <erkin.pardayev@gmail.com>
2019-10-02 15:48:37 +0800

    Generated api module

--- config/web.php
+++ config/web.php
@@ -11,6 +11,11 @@
 			'@bower' => '@vendor/bower-asset',
 			'@npm'   => '@vendor/npm-asset',
 		],
+		'modules' => [
+			'api' => [
+				'class' => 'app\modules\ApiModule',
+			],
+		],
 		'components' => [
 			'request' => [
 				// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
