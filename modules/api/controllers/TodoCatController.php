1104a93a20adb8d581b27d8cd07c8c62a0c0c7c6
Erkin Pardayev <erkin.pardayev@gmail.com>
2019-11-22 12:07:04 +0800

    Removed docs from controller

--- modules/api/controllers/TodoCatController.php
+++ modules/api/controllers/TodoCatController.php
@@ -26,18 +26,6 @@ class TodoCatController extends Controller
     return TodoCat::find()->where(['user_id' => $this->data['user']])->orderBy('id DESC')->all();
   }
 
-	/**
-	 * @return bool|string
-	 *
-	 * // Example of json
-			{
-				"token": "N6WR5900QWhX6ZovVy5mp301zBTSv9Qp",
-				"user": 1,
-				"title" : "test1",
-				"comment" : "For testing #1"
-			}
-	 */
-
   public function actionCreate()
   {
     $model = new TodoCat();
@@ -56,34 +44,12 @@ class TodoCatController extends Controller
     }
   }
 
-	/**
-	 *
-	 Example of json
-		{
-			"token": "N6WR5900QWhX6ZovVy5mp301zBTSv9Qp",
-			"user": 1,
-			"id" : 2
-		}
-	 */
-
   public function actionView()
   {
   	$id = (int) $this->data['id'];
 	  $data = TodoCat::find()->where(['id' => $id])->orderBy('id DESC')->asArray()->all();
 	  return $data;
   }
-
-	/**
-	 * @return bool
-	 * Example json
-		 {
-			 "token": "N6WR5900QWhX6ZovVy5mp301zBTSv9Qp",
-			 "user": 1,
-			 "id" : 1,
-			 "title" : "tested #1",
-			 "comment" : "For testing #1"
-		 }
-	 */
 
   public function actionUpdate()
   {
@@ -95,19 +61,6 @@ class TodoCatController extends Controller
   	$category->title = $this->data['title'];
   	return $category->save();
   }
-
-	/**
-	 *
-	 * @return bool
-	 *  Example json
-		 {
-			 "token": "N6WR5900QWhX6ZovVy5mp301zBTSv9Qp",
-			 "user": 1,
-			 "id" : 3
-		 }
-	 * @throws \Throwable
-	 * @throws \yii\db\StaleObjectException
-	 */
 
   public function actionDelete() : bool
   {
